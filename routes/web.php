98b0e6065ba52948dbdac5cffc28b490747bc68c
ari <ari3ramadhani@gmail.com>
2021-08-19 14:13:43 +0700

    Home page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    echo "This is home page";
+});
+
+Route::get('/about', function () {
+    return view('about');
+})->middleware('umur');

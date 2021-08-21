fccd0b3eeb64e1fd901b1f6d4bf0c97cf4479653
ari <ari3ramadhani@gmail.com>
2021-08-21 22:50:28 +0700

    resize

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\DB;
 use App\Models\Brand;
 use Illuminate\Support\Carbon;
 
+// resize
+use Image;
+
 
 class BrandController extends Controller
 {
@@ -30,14 +33,25 @@ class BrandController extends Controller
 
         $brand_image = $request->file('brand_image');
         
-        $name_gen = hexdec(uniqid());
-        $img_ext = strtolower($brand_image->getClientOriginalExtension());
-        $img_name = $name_gen .'.'.$img_ext;
-        $up_location = 'image/brand/';
-        $last_img = $up_location.$img_name;
+        // $name_gen = hexdec(uniqid());
+        // $img_ext = strtolower($brand_image->getClientOriginalExtension());
+        // $img_name = $name_gen .'.'.$img_ext;
+        // $up_location = 'image/brand/';
+        // $last_img = $up_location.$img_name;
+        // $brand_image->move($up_location,$img_name);
 
-        $brand_image->move($up_location,$img_name);
+        // cara resize
+        // untuk resize gambar buka link http://image.intervention.io/getting_started/installation
+        // install composer composer.phar require intervention/image
+        // tambahim di config/app        Intervention\Image\ImageServiceProvider::class
+        // sama ini dipaling bawah  config/app   'Image' => Intervention\Image\Facades\Image::class
+        // php artisan vendor:publish --provider="Intervention\Image\ImageServiceProviderLaravelRecent"
+        $name_gen = hexdec(uniqid()).'.'.$brand_image->getClientOriginalExtension();
+        Image::make($brand_image)->resize(300,200)->save('image/brand/'.$name_gen);
 
+        $last_img = 'image/brand/'.$name_gen;
+        // resize sampe sini
+        
         Brand::insert([
             'brand_name'=>$request->brand_name,
             'brand_image'=>$last_img,
@@ -90,6 +104,14 @@ class BrandController extends Controller
         }
         
         return Redirect()->back()->with('success', 'Brand berhasil diubah');
+    }
 
+    public function Delete($id){
+        $image = Brand::find($id);
+        $old_image = $image->brand_image;
+        unlink($old_image);
+
+        Brand::find($id)->delete();
+        return Redirect()->back()->with('success', 'Brand berhasil dihapus');
     }
 }
